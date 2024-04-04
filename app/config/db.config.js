9d5fb467fc8122a06ab5cacce7bb8c56627053e7
ProPy2023 <144798650+ProPy2023@users.noreply.github.com>
2024-04-04 20:27:46 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,32 @@
-module.exports = {
+/*module.exports = {
   HOST: "localhost",
   USER: "postgres",
   PASSWORD: "123",
   DB: "testdb",
   dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+*/
+
+module.exports = {
+  HOST: "dpg-co7bpEn109ks73860js0-a.oregon-postgres.render.com",
+  USER: "postgres_user",
+  PASSWORD: "J4lg1IhBM7viuX8EH6PnUKx8Q9U8xECm",
+  DB: "testdb_vnej",
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Allow self-signed certificates
+    },
+    keepAlive: true
+  },
+  ssl: true,
   pool: {
     max: 5,
     min: 0,
