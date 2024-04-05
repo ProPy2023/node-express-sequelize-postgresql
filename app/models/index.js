c4caa229a4c13bc005afb55380896f0744acdf35
ProPy2023 <144798650+ProPy2023@users.noreply.github.com>
2024-04-05 14:26:34 +0530

    Update index.js

--- app/models/index.js
+++ app/models/index.js
@@ -1,15 +1,27 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//   host: dbConfig.HOST,
+//   dialect: dbConfig.dialect,
 
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
+//   pool: {
+//     max: dbConfig.pool.max,
+//     min: dbConfig.pool.min,
+//     acquire: dbConfig.pool.acquire,
+//     idle: dbConfig.pool.idle
+//   }
+// });
+
+
+// Connection string
+const connectionString = 'postgres://postgres_user:J4lg1IhAM7viuX8EH6PnUKx8Q9U8xECm@dpg-co7bp0n109ks73860js0-a.oregon-postgres.render.com/testdb_vnej';
+
+// Initialize Sequelize with the connection string
+const sequelize = new Sequelize(connectionString, {
+  dialect: 'postgres', // Specify the dialect
+  dialectOptions: {
+    ssl: true // Enable SSL
   }
 });
 
